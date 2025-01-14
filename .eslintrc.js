785a9b9882afc8fb98f613320ab2bc80f4d632ca
keht-agta <keht-agta@yandex.ru>
2025-01-14 08:16:32 +0800

    dz1.1

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,8 @@ module.exports = {
     'ecmaVersion': 'latest',
     'sourceType': 'module'
   },
-  'rules': {}
+  'rules': {
+        "no-unused-vars": "warn",
+        "no-multiple-empty-lines": "warn"
+  }
 }
--- src/app.js
+++ src/app.js
@@ -5,4 +5,6 @@ const characters = [
   {name: 'лучник', health: 0}
 ];
 
-const alive = characters.filter(item => item.health > 0);
+// eslint-disable-next-line no-unused-vars
+const alive = characters.filter((item) => item.health > 0);
+// const alive = characters.filter(item => item.health > 0);
